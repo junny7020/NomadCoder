67545814b940b22958fda3ce24cfca176b1f578e
June Choi <junny7020@gmail.com>
2021-12-14 04:59:10 +0000

    Object

--- vanilaJS_ChromeApp/app.js
+++ vanilaJS_ChromeApp/app.js
@@ -1,15 +1,16 @@
-const nonsense = [1, 2, null, undefined, true, "hello", 23.3]
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"]
-console.log(daysOfWeek);
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
-console.log(daysOfWeek[2]);
-// const declare can not be modified
-//For const, it is like declaring a box
-// const a=2; means make a box, "a" that has 2
-// Since a is just an integer variable it can not be changed
+const player = {
+    name: "June",
+    points: 10,
+    studyMath: true,
+};
+// this is an object
 
-// but in an array the content of the box can be added
-// const toBuy =["beef","eggs"]; means make a box, "toBuy" that has beef, eggs
-// so since it is an array you can add more content in to it.
-// The box will still be the same box, but with more contents.
+console.log(player);
+console.log(player.name);
+// this means console is also an object
+console.log(player["name"]);
+console.log(player.points);
+player.points = 20;
+console.log(player.points);
+// player is const object
+// the object can not be changed but the content can be changed or added
