d4a8886d7ec775038974d36b863971c3896d8959
June Choi <junny7020@gmail.com>
2021-12-14 04:50:01 +0000

    Array, const array, push

--- vanilaJS_ChromeApp/app.js
+++ vanilaJS_ChromeApp/app.js
@@ -1,13 +1,15 @@
-const amIcoder = true;
-const thisIsNull = null;
-let something;
+const nonsense = [1, 2, null, undefined, true, "hello", 23.3]
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"]
+console.log(daysOfWeek);
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
+// const declare can not be modified
+//For const, it is like declaring a box
+// const a=2; means make a box, "a" that has 2
+// Since a is just an integer variable it can not be changed
 
-console.log(amIcoder);
-console.log(thisIsNull);
-console.log(something);
-something = false;
-console.log(something);
-
-// null is a value of nothing
-// undefined == it is declared but not defined
-// python: null-->None
+// but in an array the content of the box can be added
+// const toBuy =["beef","eggs"]; means make a box, "toBuy" that has beef, eggs
+// so since it is an array you can add more content in to it.
+// The box will still be the same box, but with more contents.
