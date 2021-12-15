4dc1b3de9f4493fb805f934c4ff2b2838905c07a
June Choi <junny7020@gmail.com>
2021-12-15 00:58:42 +0000

    Conditionals Vanila JS 14th Dec

--- vanilaJS_ChromeApp/app.js
+++ vanilaJS_ChromeApp/app.js
@@ -1,33 +1,15 @@
-const calculator = {
-    add: function(a, b){
-        return a + b;
-    },
+const age = parseInt(prompt("How old are you?: "));
 
-    minus: function(a, b){
-        return a - b;
-    },
-
-    mult: function(a, b){
-        return a * b;
-    },
-
-    div: function(a, b){
-        return a / b;
-    },
-
-    powerof: function(a, b){
-        return a ** b;
-    },
-};
-
-const addResult = calculator.add(2, 3);
-const minusResult = calculator.minus(addResult, 1);
-const multResult = calculator.mult(addResult, minusResult);
-const powResult = calculator.powerof(multResult, minusResult);
-const divResult = calculator.div(powResult, addResult);
-
-console.log(addResult);
-console.log(minusResult);
-console.log(multResult);
-console.log(powResult);
-console.log(divResult);
+if(isNaN(age) || age < 0) {
+    console.log("Please write a real positive number.");
+} else if(age < 18) {
+    console.log("You are too young.");
+} else if(age >= 18 && age <= 50) {
+    console.log("You can drink");
+} else if(age > 50 && age <= 80) {
+    console.log("You should exercise");
+} else if(age === 100) {
+    console.log("Wow! you are wise");
+} else if(age > 80) {
+    console.log("You can do whatever you want.");
+}
