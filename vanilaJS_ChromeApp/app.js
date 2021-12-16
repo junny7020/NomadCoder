a59532b8e40157c25100e3418311cd418a0c1468
June Choi <junny7020@gmail.com>
2021-12-16 05:17:59 +0000

    toggle applied

--- vanilaJS_ChromeApp/app.js
+++ vanilaJS_ChromeApp/app.js
@@ -1,15 +1,6 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClicker() { 
-    // if you use clicked(raw value) twice
-    // you can make some mistakes
-    const clickedClass = "clicked";
-    // this way you can find error easily
-    
-    if (h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
 h1.addEventListener("click", handleTitleClicker);
