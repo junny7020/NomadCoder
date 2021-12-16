01fde26d75c3c72344e54810e4cdceb4bc505fac
June Choi <junny7020@gmail.com>
2021-12-16 23:49:44 +0000

    16th Dec 2021
    PreventDefault of an event

--- vanilaJS_ChromeApp/app.js
+++ vanilaJS_ChromeApp/app.js
@@ -1,6 +1,19 @@
-const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClicker() { 
-    h1.classList.toggle("clicked");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
 }
-h1.addEventListener("click", handleTitleClicker);
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+    alert("clicked!")
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
