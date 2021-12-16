5490e53be248ed9d73c42e029f792e8b5d619fe9
June Choi <junny7020@gmail.com>
2021-12-16 04:45:59 +0000

    Vanila JS change color

--- vanilaJS_ChromeApp/app.js
+++ vanilaJS_ChromeApp/app.js
@@ -1,45 +1,13 @@
-// querySelector ---> "#Id element"
-// or ".tag element"
-// queryselectorAll -->  array
-// getElementsById --> array
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick() {
-    console.log("Title was clicked!");
-    h1.style.color = "pink";
+function handleTitleClicker() {
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    }else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
-
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("Copier!");
-}
-
-function handleWindowOffline() {
-    alert("Oh no you are disconnected, please turn on your wifi!");
-}
-
-function handleWindowOnline() {
-    alert("Yeay you are online!");
-}
-
-console.dir(h1);
-h1.style.color = "blue";
-h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClicker);
