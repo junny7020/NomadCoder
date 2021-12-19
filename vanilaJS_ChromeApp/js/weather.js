d6f3ca3d2f1526299bc905b19f653475f30ed77e
June Choi <junny7020@gmail.com>
2021-12-19 16:08:11 +0000

    19th Dec 2021 Vanila JS Weather feature added
    weather feature added using weather api.
    need to use css for making things look prettier

--- vanilaJS_ChromeApp/js/weather.js
+++ vanilaJS_ChromeApp/js/weather.js
@@ -1,7 +1,20 @@
+const API_KEY =  "41606655dd1312f106246232b71454bf"
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;;
-    console.log("You live in ", lat, lng);
+    const lon = position.coords.longitude;;
+    console.log("You live in ", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather= document.querySelector("#weather span:first-child");
+        const city= document.querySelector("#weather span:last-child");
+        const name = data.name;
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+    // fetch is promise waiting for response from server
 }
 
 function onGeoError() {
